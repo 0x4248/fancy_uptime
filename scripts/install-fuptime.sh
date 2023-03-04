46ff3a1823c489464100fbe4eedb92272daa29ce
awesomelewis2007 <awesomelewis2007@gmail.com>
2023-03-04 09:37:35 +0000

    Add comments to files

--- scripts/install-fuptime.sh
+++ scripts/install-fuptime.sh
@@ -1,4 +1,9 @@
-!#/bin/bash
+# !#/bin/bash
+# Fancy uptime make file
+# A fancy uptime reader that makes a nicer output than the uptime command. 
+# Made by: awesomelewis2007
+# Github: https://www.github.com/awesomelewis2007/fancy_uptime
+# License: GNU General Public License v3.0
 if [ "$EUID" -ne 0 ]
   then echo -e "\e[31mPlease run as root\e[0m"
   exit
