786077e34de125fa60205fecd70af04c3038b7df
awesomelewis2007 <awesomelewis2007@gmail.com>
2023-03-13 21:33:12 +0000

    Update commenting style

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-// Fancy uptime make file
-// A fancy uptime reader that makes a nicer output than the uptime command. 
-// Made by: awesomelewis2007
-// Github: https://www.github.com/awesomelewis2007/fancy_uptime
-// License: GNU General Public License v3.0
+/* Fancy uptime make file
+ * A fancy uptime reader that makes a nicer output than the uptime command. 
+ * Made by: awesomelewis2007
+ * Github: https://www.github.com/awesomelewis2007/fancy_uptime
+ * License: GNU General Public License v3.0
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
